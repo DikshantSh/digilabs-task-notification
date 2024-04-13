b571758a9bc903ee9ffa2319214514210f15034b
DikshantSh <dikshant.2412@kgpian.iitkgp.ac.in>
2024-04-13 14:29:42 +0530

    manifest-maskable-update

--- next.config.mjs
+++ next.config.mjs
@@ -7,9 +7,4 @@ import withPWA from "next-pwa";
 
 export default withPWA({
 //   reactStrictMode: true,
-//   pwa: {
-//     dest: "public",
-//     register: true,
-//     skipWaiting: true,
-//   },
 });
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,11 +1,24 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "DiGiLABS",
   description: "A Notification generator",
+  generator: "Next.js",
+  manifest: "/manifest.json",
+  themeColor: [{ media: "(dark)", color: "#fff" }],
+  keywords: ["nextjs", "nextjs13", "next13", "pwa", "next-pwa"],
+  themeColor: [
+    { media: "(prefers-color-scheme: dark)", color: "#000000" },
+    { media: "(prefers-color-scheme: light)", color: "#ffffff" },
+  ],
+  viewport:
+    "minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover",
+  icons: [
+    { rel: "apple-touch-icon", url: "icons/icon-128x128.png" },
+    { rel: "icon", url: "icons/icon-128x128.png" },
+  ],
 };
 
 export default function RootLayout({ children }) {
